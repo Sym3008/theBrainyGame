e67c29c816d2e790402dddf302f65b44ed618a8a
Sym3008 <s30@libero.it>
2021-11-04 18:02:08 +0100

    main

---
+++ src/test/java/it/simone/theBrainyGame/TheBrainyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package it.simone.theBrainyGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBrainyGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
